42740c50977fe817fa63321bb8eac4ceb213db6a
namiyama <namiyama814@gmail.com>
2026-03-06 15:47:28 +0900

    BootStrap,JSを使用したデザイン, UIの改善

---
+++ public/yap.js
@@ -0,0 +1,30 @@
+'use strict'
+//一番下を表示
+window.addEventListener('load', () => {
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+//Enter + Ctrl(Macの場合はCommand)でメッセージ送信
+const formElement = document.forms['message-form'];
+const textareaElement = formElement.elements['content'];
+
+textareaElement.addEventListener('keydown', (event) => {
+
+  if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+    event.preventDefault();
+    formElement.submit();
+  }
+
+});
+
+//送信キーを押しているか判定
+function isPressSubmitKey(event) {
+  //Windowsの処理
+  if (event.ctrlKey) {
+    return true;
+  };
+  //Macの処理(MacのCommandキーはmetaKeyという名前)
+  if (event.metaKey) {
+    return true;
+  }
+}
